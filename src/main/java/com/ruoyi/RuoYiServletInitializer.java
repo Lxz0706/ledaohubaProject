1541e0226b21cabace7965e1dd9cec35657ebc59
Lxz0706 <878524556@qq.com>
2020-05-14 17:07:19 +0800

    20200514江苏乐道胡巴项目代码第一次提交代码

---
+++ src/main/java/com/ruoyi/RuoYiServletInitializer.java
@@ -0,0 +1,22 @@
+package com.ruoyi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author ruoyi
+ */
+public class RuoYiServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(RuoYiApplication.class);
+    }
+    
+}
